b3af26a36a6aff5a62f093b5318e33798c51b83e
Anouar Boutouga <BOUTOUGA.ANOUAR@ofppt-edu.ma>
2023-01-17 16:17:11 +0100

    tp

---
+++ README.md
@@ -0,0 +1,5 @@
+[15:16] TAHA FERHANI
+1 - Créer un dossier "views"
+2 - Créer des pages html dans le dossier views
+3 - Créer un menu dans tous les pages html
+4 - Créer un serveur qui gère les pages et envoi la page demandé
---
+++ app.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const htpp = require("htpp");
+
+const server = htpp.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.readFile("./views/about.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        return res.end();
+      });
+      break;
+    case "/contact":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.readFile("./views/contact.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        return res.end();
+      });
+      break;
+    case "menu":
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("./views/menu.html", (err, data) => {
+          if (err) throw err;
+          res.write(data);
+          return res.end();
+        });
+    default:
+        res.write("404 page not found");
+        return res.end();
+  }
+});
+
+server.listen(3000);
